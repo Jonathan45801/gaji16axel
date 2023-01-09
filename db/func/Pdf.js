981d80033b490b0773ff3a67e20e4eb38c99617c
jonathan <43164516+Jonathan45801@users.noreply.github.com>
2023-01-09 21:09:06 +0700

    perubahan 9 Januari 2022

--- db/func/Pdf.js
+++ db/func/Pdf.js
@@ -15,16 +15,20 @@ const generatorheader=(doc)=>{
 }
 async function getdatafromtable(id,tanggal){
     const promisepool = config.pool.promise()
-    const[rows,fields] = await promisepool.query("select a.nama,a.jabatan,b.bulan,a.jumlah_gaji,c.gajiterlambat, "+
-    "b.santun,b.keterangan,b.terlambat,sum(d.terlambat) as terlambatabsen from tb_karyawan a inner join "+
-    "tb_laporan b on a.id = b.id_karyawan inner join tb_terlambat c on a.jabatan = c.jabatan inner join "+
-    "tb_absen d on d.id_karyawan = a.id where a.id = ? and date_format(d.tanggal,'%m-%Y') = ? GROUP by "+
-    "a.nama,a.jabatan,b.bulan,a.jumlah_gaji,c.gajiterlambat,b.santun,b.keterangan,b.terlambat",[id,tanggal])
+    const[rows,fields] = await promisepool.query("select a.nama,a.jabatan,b.bulan,a.jumlah_gaji, "+
+    "(select gajiterlambat from tb_terlambat where id = '2') as gajiterlambat, "+
+    "(select gajiterlambat from tb_terlambat where id = '1') as bayarterlambat, (select gajitam from tb_tambahan where id = '1') as uangmakan, "+
+    "b.santun,b.keterangan,sum(d.terlambat) as terlambatabsen from tb_karyawan a "+
+    "inner join tb_laporan b on a.id = b.id_karyawan inner join tb_absen d "+
+    "on d.id_karyawan = a.id where a.id = ? and date_format(d.tanggal,'%m-%Y') = '01-2023' "+
+    "GROUP by a.nama,a.jabatan,b.bulan,a.jumlah_gaji,b.santun,b.keterangan;",[id,tanggal])
     return rows;
 }
-const generatorbody=(doc,nama,bulan,jabatan,jumlahgaji,gajiterlambat,santun,keterangan,terlambat,terlambatabsen)=>{
-    let duitterlambat = 0;
-    let tanpaketerangan = 0;
+const generatorbody=(doc,nama,bulan,jabatan,jumlahgaji,gajiterlambat,bayarterlambat,uangmakan,santun,keterangan,terlambat)=>{
+    let duitterlambat = parseFloat(bayarterlambat * terlambat);
+    let tanpaketerangan = parseFloat(keterangan * gajiterlambat);
+    var jumlah_gaji = parseFloat(jumlahgaji);
+    var uang_makan = parseFloat(uangmakan);
     let pph = 25000;
     let total = 0;
     doc.text(`Nama: `+nama,110,75).fontSize(10)
@@ -35,16 +39,18 @@ const generatorbody=(doc,nama,bulan,jabatan,jumlahgaji,gajiterlambat,santun,kete
     doc.moveTo(awalpage,100).lineTo(akhirpage,100).stroke()
     doc.text(`Sistem Pembayaran : CASH`,110,120).fontSize(10)
     doc.text(`Gaji Pokok : ${separatorrupiah(jumlahgaji)}`, 350,130).fontSize(10)
-    if(terlambatabsen > terlambat)
-    {
-        duitterlambat = gajiterlambat * terlambat
-    }
-    doc.text(`Terlambat : ${separatorrupiah(duitterlambat)}`,350,140).fontSize(10)
-    doc.text(`tanpa Keterangan: ${separatorrupiah(tanpaketerangan)}`,350,150).fontSize(10)
-    doc.text(`PPH: ${separatorrupiah(pph)}`,350,160).fontSize(10)
-    doc.moveTo(350,170).lineTo(550,170).stroke();
-    total = jumlahgaji - duitterlambat - tanpaketerangan - pph;
-    doc.text(`Total: ${separatorrupiah(total)}`,350,180).fontSize(10)
+    doc.text(`Makan / Transportasi : ${separatorrupiah(uangmakan)}`, 350,140).fontSize(10)
+    // if(terlambatabsen > terlambat)
+    // {
+    //     duitterlambat = gajiterlambat * terlambat
+    // }
+    doc.text(`Terlambat : ${separatorrupiah(duitterlambat)}`,350,150).fontSize(10)
+    doc.text(`tanpa Keterangan: ${separatorrupiah(tanpaketerangan)}`,350,160).fontSize(10)
+    doc.text(`PPH: ${separatorrupiah(pph)}`,350,170).fontSize(10)
+    doc.moveTo(350,180).lineTo(550,180).stroke();
+    total = jumlah_gaji + uang_makan - duitterlambat - tanpaketerangan - pph;
+    
+    doc.text(`Total: ${separatorrupiah(total)}`,350,190).fontSize(10)
     doc.moveTo(50,200).lineTo(550,200).stroke();
 }
 const generatorakhir=(doc,nama)=>{
@@ -62,7 +68,8 @@ exports.pdfjadi=async(id,tanggal)=>{
     generatorheader(output)
     output.moveDown();
     await getdatafromtable(id,tanggal).then(b=> 
-        generatorbody(output,b[0].nama,b[0].bulan,b[0].jabatan,b[0].jumlah_gaji,b[0].gajiterlambat,b[0].santun,b[0].keterangan,b[0].terlambat,b[0].terlambatabsen)
+        generatorbody(output,b[0].nama,b[0].bulan,b[0].jabatan,b[0].jumlah_gaji,b[0].gajiterlambat,b[0].bayarterlambat,b[0].uangmakan,
+            b[0].santun,b[0].keterangan,b[0].terlambatabsen)
     )
     output.moveDown();
     await getdatafromtable(id,tanggal).then(b=> 
